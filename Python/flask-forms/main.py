9e6b97a797c009cb3d8dbdd5e29a80df63b6816b
Dkeagle <taildeman_dylan@outlook.com>
2022-06-29 10:48:07 +0200

    form now check empty fields before validating

--- Python/flask-forms/main.py
+++ Python/flask-forms/main.py
@@ -3,21 +3,49 @@ from flask import Flask, render_template, redirect, request, url_for, flash
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'd7a70bcb88e5c190838b4c12fbf0a26efbf328bcf55ebdf5'
 
+def sanitize(post_content):
+    """sanitize every input from the form, returns True when done"""
+    pc = post_content
+    return True
+
+def check(post_content):
+    """check every input from the form and flash error message when needed, returns True when done"""
+    pc = post_content
+    err = 0
+    if not pc.form["first_name"]:
+        flash("fn_empty")
+        err += 1
+    if not pc.form["last_name"]:
+        flash("ln_empty")
+        err += 1
+    if not pc.form["mail"]:
+        flash("mail_empty")
+        err += 1
+    if not pc.form["gender"]:
+        flash("gender_empty")
+        err += 1
+    if not pc.form.get("country"):
+        flash("country_empty")
+        err += 1
+    if not pc.form.get("reason"):
+        flash("reason_empty")
+        err += 1
+    if not pc.form["content"]:
+        flash("content_empty")
+        err += 1
+    return False if err else True
+
 @app.route('/', methods=['GET', 'POST'])
 def main():
+    """main function, executed when people access the main page"""
     if request.method == 'POST':
-        first_name = request.form['first_name']
-        last_name = request.form['last_name']
-        mail = request.form['mail']
-        country = request.form.get('country')
-        gender = request.form['gender']
-        reason = request.form['reason']
-        content = request.form['content']
-
-        return redirect(url_for('thanks',first_name=first_name, code=307))
+        if check(request):
+            if sanitize(request):
+                return redirect(url_for('thanks'), code=307)
 
     return render_template("form.html")
 
 @app.route('/thanks', methods=['POST'])
 def thanks():
+    """thanks function display the 'thanks for contacting us' page when the form is send and valid"""
     return render_template("thanks.html")
